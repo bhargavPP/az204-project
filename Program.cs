d662e8cdbb590570a6681e29fb6fe1aecb01e254
Bhargav <bhargavpatel2022@gmail.com>
2026-05-07 15:12:25 -0400

    Initial commit with CI/CD Workflow

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Azure.Storage.Blobs;
+using Delivery.Api.Service;
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(); // FIX: Adds support for [ApiController] and Controllers
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Optional: Customize API info (Title, Version, etc.)
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "API for testing purposes"
+    });
+});
+// ADD THIS LINE
+builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton<OrderService>();
+builder.Services.AddSingleton(x => new BlobServiceClient(builder.Configuration["StorageConnection"]));
+builder.Services.AddScoped<BlobService>();
+var app = builder.Build();
+
+app.UseSwagger();   // Serves the raw JSON file (swagger.json)
+app.UseSwaggerUI(); // Serves the visual HTML page (index.html)
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+ 
